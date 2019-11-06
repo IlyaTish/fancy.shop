7ee2840a27bab35304a81a04f6ce127528c189c7
Ilya <ilya.tishencko676@yandex.ru>
2019-11-06 18:14:43 +0300

    Did hover navigation function for product-items

--- app/catalog/view/theme/fancy.shop/js/common.js
+++ app/catalog/view/theme/fancy.shop/js/common.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  // ====== Swiper slider options ====== //
+  /*
+    Banner slider on home page ('.banner')
+  */
   var s1 = new Swiper ('.s1', {
     loop: true,
     autoplay: {
@@ -11,6 +15,10 @@ $(document).ready(function() {
     },
   });
 
+
+  /*
+    Navigation list on main page ('.slider-catalog')
+  */
   var s2 = new Swiper ('.s2', {
     loop: false,
     autoplay: {
@@ -25,11 +33,14 @@ $(document).ready(function() {
     },
   });
 
+
+  /*
+    Products ('.products')
+  */
   var s3 = new Swiper ('.s3', {
     loop: true,
     autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
+      delay: 5000
     },
     freeMode: true,
     freeModeMomentumRatio: 0.1,
@@ -42,11 +53,14 @@ $(document).ready(function() {
     }
   });
 
+
+  /*
+    Products ('.products')
+  */
   var s4 = new Swiper ('.s4', {
     loop: true,
     autoplay: {
-      delay: 8000,
-      disableOnInteraction: false
+      delay: 8000
     },
     freeMode: true,
     freeModeMomentumRatio: 0.1,
@@ -59,11 +73,14 @@ $(document).ready(function() {
     }
   });
 
+
+  /*
+    Products ('.products')
+  */
   var s5 = new Swiper ('.s5', {
     loop: true,
     autoplay: {
-      delay: 10000,
-      disableOnInteraction: false
+      delay: 10000
     },
     freeMode: true,
     freeModeMomentumRatio: 0.1,
@@ -74,5 +91,47 @@ $(document).ready(function() {
       nextEl: '.for-moms__btn-next',
       prevEl: '.for-moms__btn-prev',
     }
+  });
+
+
+  /*
+    Product images fade effect ('.product-item__img-cont')
+  */
+  var productImages = new Swiper('.product-item__img-cont.swiper-container', {
+    loop: true,
+    autoplay: false,
+    direction: 'horizontal',
+    effect: 'fade',
+    spaceBetween: 0,
+    pagination: {
+      el: '.product-item__img-cont .swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.product-item__img-cont .swiper-button-next',
+      prevEl: '.product-item__img-cont .swiper-button-prev',
+    }
+  });
+
+
+  // ====== Change images in product-item ====== //
+
+  // var id = 0;
+
+  // $('.products .product-item__img-cont').hover(
+  //   /* Stuff to do when the mouse enters the element */
+  //   function(el) {
+  //   var el = $(this);
+  //   id = setInterval(function() {
+  //     el.find($('.swiper-button-next')).trigger('click');
+  //   }, 1000);
+  // }, function(el) {
+  //   /* Stuff to do when the mouse leaves the element */
+  //   clearInterval(id);
+  // });
+
+  $('.swiper-pagination-bullet').on('mouseenter', function() {
+    $(this).trigger('click');
+    console.log('Курсор прошел');
   });
 });
